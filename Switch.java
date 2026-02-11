3ab534957ecd4886f587396dcc9a12b7af348fd8
Raja Naveen <your-email@gmail.com>
2026-02-11 16:46:49 +0530

    intial commits

---
+++ Switch.java
@@ -0,0 +1,49 @@
+import java.util.*;
+public class Switch {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the a,b values:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("1.Add");
+        System.out.println("2.Subtraction");
+        System.out.println("3.multiplication");
+        System.out.println("4.Division");
+        System.out.println("5.modulus");
+        System.out.println("6.Exit");
+        System.out.println("Enter your choice");
+        int c=sc.nextInt();
+        switch(c)
+        {
+            case 1:
+                System.out.println("A+B:"+(a+b));
+                break;
+            case 2:
+                System.out.println("A-B:"+(a-b));            
+                    break;
+
+            case 3:
+                System.out.println("AXB:"+(a*b));
+                                break;
+
+            case 4:
+                System.out.println("A/B:"+(a/b));
+                                break;
+
+            case 5:
+                System.out.println("A%B:"+(a%b));
+                                break;
+
+            case 6:
+                System.out.println("Exit");
+                                break;
+
+            default:
+                System.out.println("Your choice is out of range");
+        }
+
+
+    }
+
+    
+}
